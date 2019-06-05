a52da1f9fbe3e5a3df9720134a841608d26c98cc
Austin Smith <austinsmith7531@gmail.com>
2019-06-04 21:21:40 -0400

    aslkdjfs;oks

--- models/news.js
+++ models/news.js
@@ -1,18 +1,18 @@
 module.exports = function(sequelize, DataTypes) {
-  var News_prefs = sequelize.define("News_prefs", {
-    first_name: {
+  var Newsprefs = sequelize.define("Newsprefs", {
+    country: {
       type:DataTypes.STRING,
       allowNull:false,
       len:[1,50]
     },
   });
-  News_prefs.associate = function (models) {
-    models.News_prefs.belongsTo(models.Users, {
+  Newsprefs.associate = function (models) {
+    models.Newsprefs.belongsTo(models.Users, {
       onDelete: "CASCADE",
       foreignKey: {
         allowNull: true
       }
     });
   }
-  return News_prefs;
+  return Newsprefs;
 };
--- server.js
+++ server.js
@@ -43,5 +43,4 @@ db.sequelize.sync(syncOptions).then(function() {
     );
   });
 });
-
 module.exports = app;
