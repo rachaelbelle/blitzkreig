8fb79bbb183e791fb8006d326065ae3db4ffb230
DaniBlaney <daniblaney@gmail.com>
2019-06-06 20:31:40 -0400

    fixed html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,17 +3,26 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({})
-    });
-
+    db.Example.findAll({});
+    {
+      res.render("index");
+    }
+  });
 
   // Load example page and pass in an example by id
-  app.get("preferences", function(req, res) {
-    db.Example.findOne({ })
-     {
-      res.render("preferences")
+  app.get("/preferences", function(req, res) {
+    db.Example.findAll({});
+    {
+      res.render("preferences");
+    }
+  });
 
-    };
+  // Load example page and pass in an example by id
+  app.get("/userProfile", function(req, res) {
+    db.Example.findAll({});
+    {
+      res.render("userProfile");
+    }
   });
 
   // Render 404 page for any unmatched routes
