9bab44e98efb6e934df696620625b60135613718
davidmbedford <47337007+davidmbedford@users.noreply.github.com>
2019-06-08 14:56:47 -0400

    Update seed.sql

--- models/seed.sql
+++ models/seed.sql
@@ -1,16 +1,1 @@
-USE blitzdb;
 
-CREATE TABLE Users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  firstName VARCHAR(50),
-  lastName VARCHAR(50),
-  email VARCHAR(50),
-  password VARCHAR(50),
-  zipcode INT(5),
-  weather BOOLEAN,
-  traffic BOOLEAN,
-  quotes BOOLEAN
-);
-
-INSERT INTO blitzdb (firstName, lastName, email, password, zipCode, weather, traffic, quotes)
-VALUES ("Charlie", "Fake", "fake@gmail.com", "null", "19148", true, true, true, true);
