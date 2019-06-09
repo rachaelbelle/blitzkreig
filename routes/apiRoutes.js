418553af867200b33d4b345a904254b7d816498f
Austin Smith <austinsmith7531@gmail.com>
2019-06-08 21:43:50 -0400

    fixed the api-routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -28,19 +28,35 @@ module.exports = function(app) {
   //     });
   // });
 
-  // Create a new example
+  // Create a new user
   app.post("/api/users", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
 
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExample) {
-      //console.log(dbExample);
-      res.json(dbExample);
-    });
+// Create a new example
+app.post("/api/examples", function(req, res) {
+  db.Example.create(req.body).then(function(dbExample) {
+    res.json(dbExample);
   });
+});
+
+app.get("/api/examples", function(req, res) {
+  db.Example.findAll({}).then(function(dbExample) {
+    //console.log(dbExample);
+    res.json(dbExample);
+  });
+});
+
+// Delete an example by id
+app.delete("/api/examples/:id", function(req, res) {
+  db.Example.destroy({ where: { id: req.params.id } }).then(function(
+    dbExample
+  ) {
+    res.json(dbExample);
+  });
+});
 
   // Delete an example by id
   app.delete("/api/examples/:id", function(req, res) {
