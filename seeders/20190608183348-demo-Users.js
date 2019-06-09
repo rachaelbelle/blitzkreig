14f542693575ce799cba2e0ec0c8f0194380f3de
YvelStLaurent <rachaelbelle@gmail.com>
2019-06-08 21:44:29 -0400

    travis fixes - adding migrations

--- seeders/20190608183348-demo-Users.js
+++ seeders/20190608183348-demo-Users.js
@@ -1,20 +1,25 @@
-'use strict';
+"use strict";
 
 module.exports = {
+  // eslint-disable-next-line no-unused-vars
   up: (queryInterface, Sequelize) => {
-    
-      
-      return queryInterface.bulkInsert('Users', [{
-        firstName: 'John',
-        lastName:"Doe",
-        email:"johndoe@gmail.com",
-        password:"********",
-        isBetaMember: false
-      }], {});
-    
+    return queryInterface.bulkInsert(
+      "Users",
+      [
+        {
+          firstName: "John",
+          lastName: "Doe",
+          email: "johndoe@gmail.com",
+          password: "********",
+          isBetaMember: false
+        }
+      ],
+      {}
+    );
   },
 
+  // eslint-disable-next-line no-unused-vars
   down: (queryInterface, Sequelize) => {
-      return queryInterface.bulkDelete('Users', null, {});
+    return queryInterface.bulkDelete("Users", null, {});
   }
 };
