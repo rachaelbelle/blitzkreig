2483b7f8ea27a8f83226045f48fe2e4a437afbf7
John Kozyrakis <jckozy86@gmail.com>
2019-06-13 15:32:30 -0400

    quotes work ok, news is pasting string... have issues with news obj and handlebars

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,8 +1,15 @@
 var db = require("../models");
+const NewsAPI = require("newsapi");
+const newsapi = new NewsAPI("1b52f242b6544eddba125c9fb88612e1");
+const Request = require("request");
+var weather = require("weather-js");
+const googleMapsClient = require("@google/maps").createClient({
+  key: "AIzaSyA4GvQEVUhN2cbBZvQ1ObqGmRnup1mXPyA"
+});
 
-module.exports = function(app) {
+module.exports = function (app) {
   // Load index page
-  app.get("/", function(req, res) {
+  app.get("/", function (req, res) {
     console.log("htmlroutes.js In get /");
     // db.users.findAll({});
     // {
@@ -11,69 +18,143 @@ module.exports = function(app) {
   });
 
   // Load example page and pass in an example by id
-  app.get("/preferences", function(req, res) {
+  app.get("/preferences", function (req, res) {
     console.log("htmlroutes.js In get preferences");
-    db.users.findAll({});
-    {
+    db.users.findAll({}); {
       res.render("preferences");
     }
   });
 
   // Load example page and pass in an example by id
-  app.get("/userProfile/:name", function(req, res) {
+  app.get("/userProfile/:name", function (req, res) {
     console.log("htmlroutes.js In get /userProfile with body");
     //console.log("*******************");
     //console.log(req.body);
     console.log("Username is: ");
     console.log(req.params.name);
 
-    // let myUser = req.body;
+    let quote;
+    let titles=[];
+    let titletrying="";
+    let articles=[];
+    let articlestrying="";
 
-    // myUser = {
-    //   id: 1,
-    //   userName: "guest",
-    //   weather: true,
-    //   news: true,
-    //   traffic: true,
-    //   quotes: true
-    // };
-    // console.log("User being sent is: ");
-    // console.log(myUser);
+    // $.get("/quote", {}
+    // ).then( data => {
+    //   debugger;
+    //   quote=data;
+    // });
+    Request.get(
+      "http://quotes.stormconsultancy.co.uk/random.json",
+      (error, response, body) => {
+        if (error) {
+          quote = "Error getting quote... Sorry";
+        }
+        debugger;
+        //console.log(JSON.parse(body));
+        quote = JSON.parse(body);
+        
+        //got the quote going... need to add the next part
 
-    db.users
-      .findOne({
-        where: {
-          userName: req.params.name
-        }
-      })
-      .then(function(dbData) {
-        let myUser = dbData.dataValues;
-        console.log("My user passed is: ");
-        console.log(myUser);
-        if( myUser.userName == "guest"){
-          myUser.weather = true;
-          myUser.news = true;
-          myUser.traffic = true;
-          myUser.quotes = true;
-        }
-        //console.log(dbData);
-        console.log(myUser.userName);
-        console.log(myUser.weather);
-        console.log(myUser.news);
-        console.log(myUser.traffic);
-        console.log(myUser.quotes);
-        res.render("userProfile", myUser);
-      });
+        newsapi.v2.topHeadlines({
+          // q: 'trump',
+          // category: 'politics',
+          // language: 'en',
+          country: 'us'
+        }).then(response => {
+          //console.log(response);
+
+          let count=0;
+          let count2=0;
+          //articles = response.articles;
+          console.log("**********************");
+          //console.log(response);
+          response.articles.forEach(article => {
+            var jsonTitle;
+            if(count!==3){
+              jsonTitle = {
+                title: article.title
+              }
+              titletrying+="<p>"+article.title+"</p>";
+              //titles.push(jsonTitle);
+              titles.push(titletrying);
+              //console.log("added title: ");
+              //console.log(jsonTitle)
+              count++;
+            }
+            var jsonArticle;
+            if(count2!==10){
+              jsonArticle = {
+                title: article.title,
+                description: article.description,
+                url: article.url
+              }
+              articlestrying+="<p>"+article.title+"</p>";
+              articlestrying+="<p>"+article.description+"</p>";
+              articlestrying+="<p>"+article.url+"</p>";
+              //articles.push(jsonArticle);
+              articles.push(articlestrying);
+              //console.log("added article: ");
+              //console.log(jsonArticle)
+              count2++;
+            }
+          });
+
+          db.users
+          .findOne({
+            where: {
+              userName: req.params.name
+            }
+          })
+          .then(function (dbData) {
+            let myUser = dbData.dataValues;
+            console.log("My user passed is: ");
+            console.log(myUser);
+            if (myUser.userName == "guest") {
+              myUser.weather = true;
+              myUser.news = true;
+              myUser.traffic = true;
+              myUser.quotes = true;
+            }
+            myUser.quote = quote;
+            // var hbsObject1 = {
+            //   titles: titles
+            // };
+            // var hbsObject2 = {
+            //   articles: articles
+            // };
+            myUser.titles = titletrying;
+            myUser.articles = articlestrying;
+            console.log("sending my user: ");
+            console.log(myUser);
+            res.render("userProfile", myUser);
+          })
+
+
+
+
+        });
+        
+        
+        
+
+
+
+      }
+    )
+
+
+
   });
 
-  app.post("/api/users", function(req, res) {
+  app.post("/api/users", function (req, res) {
     console.log("htmlroutes.js /api/users");
     //console.log(req);
     res.json(req.body);
   });
 
   // Render 404 page for any unmatched routes
-  app.get("*", function(req, res) {
+  app.get("*", function (req, res) {
     console.log("htmlroutes.js In get *");
     res.render("404");
   });
